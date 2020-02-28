f775d8e23400cbd7db979e119503073c62eedc9e
Gabor Babicz <gabor.babicz@gmail.com>
2020-02-28 11:40:54 +0100

    Initial commit 👋

---
+++ index.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+
+console.log('');
+console.log(chalk.green(`👋  Hey there! I'm Gabor Babicz, a UX Developer based in Budapest.`));
+console.log(chalk.cyan(`✉️  Contact me via email at`, chalk.underline('gabor.babicz@gmail.com')));
+console.log('');
+console.log(chalk.blue(`🌞  Have a wonderful day!  ❤️`));
+console.log('');
