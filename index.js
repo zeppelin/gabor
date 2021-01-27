08403ff6a4b3ec364ce40f39366f4b60b51b3242
Gabor Babicz <gabor.babicz@gmail.com>
2021-01-27 21:41:44 +0100

    Update email

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 
 console.log('');
 console.log(chalk.green(`👋  Hey there! I'm Gabor Babicz, a UX Developer based in Budapest.`));
-console.log(chalk.cyan(`✉️  Contact me via email at`, chalk.underline('gabor.babicz@gmail.com')));
+console.log(chalk.cyan(`✉️  Contact me via email at`, chalk.underline('gabor@gaborbabicz.com')));
 console.log('');
 console.log(chalk.blue(`🌞  Have a wonderful day!  ❤️`));
 console.log('');
